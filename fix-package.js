88709efeaace0432360180d095ace7860276682b
lferreira07 <luis.ferreira@25friday.com>
2025-01-08 11:49:14 -0100

    Move Angular dependencies to peerDependencies and refine

---
+++ fix-package.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const packagePath = path.resolve(__dirname, '../dist/@angular-material-components/datetime-picker/package.json');
+const packageJson = require(packagePath);
+
+packageJson.peerDependencies = {
+  "@angular/animations": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/cdk": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/common": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/compiler": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/core": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/forms": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/material": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/platform-browser": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/platform-browser-dynamic": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "@angular/router": "^15.0.0 || ^16.0.0 || ^18.0.0",
+  "rxjs": "^6.6.3 || ^7.8.0"
+};
+
+fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
+console.log('Updated peerDependencies in package.json');
